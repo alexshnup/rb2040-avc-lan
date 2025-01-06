cf5473fe32c7d6f02a924b6b54615075b5675936
alexshnup <alexshnup@gmail.com>
2025-01-05 23:49:59 -0500

    differential line side 2

--- main copy.c
+++ main copy.c
@@ -4,22 +4,21 @@
 #include "hardware/clocks.h"
 #include "avc_lan_capture.pio.h" // Включаем сгенерированный заголовочный файл
 
-const uint pin_tx_high = 2;
-const uint pin_tx_low = 3;  // второй инвертированный пин нельзя перенанзначить он всегда +1 (следующий)
-const uint pin_rx = 4;
+const uint pin_tx = 2;
+const uint pin_rx = 3;
 
 
 int main() {
     stdio_init_all();
 
     PIO pio = pio0;
-    uint sm_tx = 0;
-    uint sm_rx = 1;
-    // int sm_rx = pio_claim_unused_sm(pio, true);
+    // uint sm_tx = 0;
+    // uint sm_rx = 1;
+    int sm_rx = pio_claim_unused_sm(pio, true);
 
-    uint offset_tx = pio_add_program(pio, &differential_manchester_tx_program);
-    uint offset_rx = pio_add_program(pio, &differential_manchester_rx_program);
-    printf("Transmit program loaded at %d\n", offset_tx);
+    // uint offset_tx = pio_add_program(pio, &differential_manchester_tx_program);
+    uint offset_rx = pio_add_program(pio, &test_program);
+    // printf("Transmit program loaded at %d\n", offset_tx);
     printf("Receive program loaded at %d\n", offset_rx);
 
     // // Configure state machines, set bit rate at 5 Mbps
@@ -33,40 +32,28 @@ int main() {
     // Configure state machines, set bit rate at 20 KHz
     // differential_manchester_tx_program_init(pio, sm_tx, offset_tx, pin_tx, 195.3125);
     // differential_manchester_rx_program_init(pio, sm_rx, offset_rx, pin_rx, 195.3125);
-
-    differential_manchester_tx_program_init(pio, sm_tx, offset_tx, pin_tx_high, 100);
-    differential_manchester_rx_program_init(pio, sm_rx, offset_rx, pin_rx, 100);
-
-//    test_program_init(pio, sm_rx, offset_rx, pin_rx, 1);
+    // differential_manchester_rx_program_init(pio, sm_tx, offset_tx, pin_tx, 100);
+   test_program_init(pio, sm_rx, offset_rx, pin_rx, 1);
 
 
     // differential_manchester_rx_program_init(pio, sm_rx, offset_rx, pin_rx, 16);
 
 
-    pio_sm_set_enabled(pio, sm_tx, false);
+    // pio_sm_set_enabled(pio, sm_tx, false);
 
+    // pio_sm_set_enabled(pio, sm_tx, true);
 
-    while (true) {
+    // while (true) {
 
-        pio_sm_put_blocking(pio, sm_tx, 0);
-        pio_sm_set_enabled(pio, sm_tx, true);
-        // printf("%08x\n", pio_sm_get_blocking(pio, sm_rx));
-        sleep_ms(1000);
+    //     pio_sm_put_blocking(pio, sm_tx, 0);
+    //     pio_sm_put_blocking(pio, sm_tx, 0x0ff0a55a);
+    //     pio_sm_put_blocking(pio, sm_tx, 0x12345678);
 
-        pio_sm_put_blocking(pio, sm_tx, 0x01);
-        pio_sm_set_enabled(pio, sm_tx, true);
-        // printf("%08x\n", pio_sm_get_blocking(pio, sm_rx));
-        sleep_ms(1000);
-
-        pio_sm_put_blocking(pio, sm_tx, 0x02);
-        pio_sm_set_enabled(pio, sm_tx, true);
-        // printf("%08x\n", pio_sm_get_blocking(pio, sm_rx));
-        sleep_ms(1000);
-
-
-
-
-    }
+    //     for (int i = 0; i < 3; ++i)
+    //         printf("%08x\n", pio_sm_get_blocking(pio, sm_rx));
+    
+    //     sleep_ms(100);
+    // }
 
         // bool started = false;
         // while (true) {
@@ -88,10 +75,10 @@ int main() {
         //     // }
         // }
 
-        // while (true) {
-        //     if (!pio_sm_is_rx_fifo_empty(pio, sm_rx)) {
-        //         uint32_t cycles = pio_sm_get_blocking(pio, sm_rx);
-        //         printf("PIO us: %u\n", cycles/57);
-        //     }
-        // }
+        while (true) {
+            if (!pio_sm_is_rx_fifo_empty(pio, sm_rx)) {
+                uint32_t cycles = pio_sm_get_blocking(pio, sm_rx);
+                printf("PIO us: %u\n", cycles);
+            }
+        }
 }
